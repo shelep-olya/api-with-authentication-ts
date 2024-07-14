c16336e73f62fd73fecb94d70e0e3b335756c3aa
shelep-olya <shelep.olya@gmail.com>
2024-07-14 12:18:04 +0300

    db connected, added user model

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({path: "./config.env"});
+
+const MONGO_URL = process.env.MONGODB_URI;
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+    console.log("Server is running on port 8000.");
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL) ;
+mongoose.connection.on('error', (error:Error) => {
+    console.log(error);
+})
